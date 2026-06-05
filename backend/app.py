981840fd25355f50064ca4bec320e1868e61d481
JEEL PATEL <s22cpjeel107@gmail.com>
2026-06-05 15:58:29 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -14,8 +14,9 @@ CORS(app, origins="*")
 
 # Email Configuration
 app.config["MAIL_SERVER"] = "smtp.gmail.com"
-app.config["MAIL_PORT"] = 587
-app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USE_TLS"] = False
+app.config["MAIL_USE_SSL"] = True
 app.config["MAIL_USERNAME"] = os.environ.get("GMAIL", "")
 app.config["MAIL_PASSWORD"] = os.environ.get("GMAIL_PASS", "")
 app.config["MAIL_DEFAULT_SENDER"] = os.environ.get("GMAIL", "")
